4df18e26adeac0fae5bafdfce11b806b4757fdd3
Zaky <zakyabdurrahman10@gmail.com>
2022-01-12 18:18:01 +0700

    finished event listener

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ const player = (name, item) => {
 
 const gameBoard = (() => {
     let board = [];
-    //fill the board and add event listener to each of them
+    //fill the board
     const init = () => {
+        //clear previous leftover
+        if (board.length !== 0) {
+            board.splice(0, 3);
+        }
+
         for(let i = 0; i < 3; i++) {
             let row = []
             for(let j = 0; j < 3; j++) {
@@ -19,34 +24,62 @@ const gameBoard = (() => {
         }
         console.log(board);
     }
+
+    //add checker for gameOver
     return {init, board}; 
 })();
 
 const displayController = (() =>{
-    const refreshBoard = (board) => {
+    const init = () => {
         //clean up existing elements
-        htmlGameBoard.children.forEach(e => e.remove());
+        if (htmlGameBoard.children.length !== 0) {
+            htmlGameBoard.children.forEach(e => e.remove());
+        }
 
-        //add grid based on the board arrray and add event listener for click
-    }
-    return {refreshBoard};
-})
-
-/* for(let i = 0; i < 3; i++) {
-            let row = []
+        //add grid based on the board arrray 
+        for(let i = 0; i < 3; i++) {
+            
             for(let j = 0; j < 3; j++) {
                 const grid = document.createElement('div');
                 grid.classList.add('gameboard-grid');
-                grid.setAttribute('data-item', 'blank');
+                grid.setAttribute('data-i-index', i);
+                grid.setAttribute('data-j-index', j);
                 htmlGameBoard.appendChild(grid);
-                board.push(grid);
+                
+                
             }
-            board.push(row);
-        } */
+        }
+        //and add event listener for click
+        let grids = document.querySelectorAll('.gameboard-grid');
+        grids.forEach(el => el.addEventListener('click', e => {
+            console.log(e.target);
+            const turnDisplay = document.querySelector('#player-turn');
+            
+            let i = e.target.getAttribute('data-i-index');
+            let j = e.target.getAttribute('data-j-index');
+            if (gameBoard.board[i][j] === '') {
+                gameBoard.board[i][j] = currentPlayer.item;
+                e.target.classList.add(currentPlayer.item);
+                console.table(gameBoard.board);
+            }
+            //change turn
+            if (currentPlayer == playerOne) {
+                currentPlayer = playerTwo;
+            } else {
+                currentPlayer = playerOne;
+            }
+            turnDisplay.textContent = `${currentPlayer.name} Turn`;
+        })
+        )
+    }
+    return {init};
+})()
 
-const playerOne = player('player 1', 'o');
-const playerTwo = player('player 2', 'x');
+
+
+const playerOne = player('Player 1', 'o');
+const playerTwo = player('Player 2', 'x');
 let currentPlayer = playerOne;
 
 gameBoard.init();
-console.log(gameBoard.board[0][1]);
+displayController.init();
